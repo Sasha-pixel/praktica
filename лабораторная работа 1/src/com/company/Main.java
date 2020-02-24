dd9bfbd1aac39320f7d8d4c2c5c12cf48c17291b
Sasha-Pixel <al.kanenckoff@yandex.ru>
2020-02-24 23:01:11 +0300

    лаборатрная работа №2

--- лабораторная работа 1/src/com/company/Main.java
+++ лабораторная работа 1/src/com/company/Main.java
@@ -1,54 +1,50 @@
 package com.company;
-
-import java.lang.String;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.print("Введите через пробел количество купюр различного номинала: 30 рублей, 100 рублей, 500 рублей, 1000 рублей \n");
+    public static void main(String arr[]){
         Scanner in = new Scanner(System.in);
-        int n_30 = in.nextInt();
-        int n_100 = in.nextInt();
-        int n_500 = in.nextInt();
-        int n_1000 = in.nextInt();
-        int i = 0, j = 0, y = 0, e = 0, tr = 0, s = 0, ps = 0, t = 0;
-        int max_sum = 30 * n_30 + 100 * n_100 + 500 * n_500 * 1000 * n_1000;
-
-        System.out.print("Введите нужную сумму \n");
-        int sum = in.nextInt();
-        if (sum < 10 || sum > max_sum || sum % 10 > 0)
+        String S;
+        System.out.println("Введите комбинацию скобок: ");
+        S=in.nextLine();
+        char []ar=S.toCharArray();
+        Stack stack=new Stack<Character>();
+        if(ar[0] == ')' || ar[0] == ']' || ar[0] == '}' || ar.length % 2 != 0)
         {
-            System.out.print("Данную сумму невозможно выдать. Обратитесь в другой банкомат.\n");
+            System.out.println("Неправильная комбинация");
+            return;
         }
-        else
+        for(int i = 0;i<ar.length;i++)
         {
-
-            for (i = 0;i < n_30 + 1;i++)
+            switch (ar[i])
             {
-                for (j = 0;j < n_100 + 1;j++)
-                {
-                    for (y = 0;y < n_500 + 1;y++)
-                    {
-                        for (e = 0;e < n_1000 + 1;e++)
-                        {
-                            if (i * 30 + j * 100 + y * 500 + e * 1000 == sum)
-                            {
-                                tr=i;s=j;ps=y;t=e;
-                                i=n_30+1;j=n_100+1;y=n_500+1;e=n_1000+1;sum=0;
-                            }
-                        }
+                case '(' : stack.push(ar[i]); break;
+                case '[' : stack.push(ar[i]); break;
+                case '{' :stack.push(ar[i]); break;
+                case ')' : {
+                    if (stack.size()!=0) {
+                        if (stack.peek().equals('('))
+                            stack.pop();
                     }
-                }
+                } break;
+                case '}' : {
+                    if(stack.size()!=0) {
+                        if (stack.peek().equals('{'))
+                            stack.pop();
+                    }
+                } break;
+                case ']' : {
+                    if(stack.size()!=0) {
+                        if (stack.peek().equals('['))
+                            stack.pop();
+                    }
+                } break;
             }
-            if (sum == 0)
-            {
-                System.out.print("Вы получите: " + t+" 1000-рублёвых купюр(ы), "+ ps+" 500-рублёвых купюр(ы), "+ s +" 100-рублёвых купюр(ы), "+ tr+" 30-рублёвых купюр(ы)." );
-            }
-            else
-                System.out.print("Данную сумму невозможно выдать.\n");
         }
-
+        if(stack.size()==0)
+            System.out.println("Введена правильная комбинация скобок");
+        else
+            System.out.println("Введена неправильная комбинация скобок");
     }
 }
