a89558b57556e371d6c50256b2f46912bf0e5598
Sasha-Pixel <al.kanenckoff@yandex.ru>
2020-03-09 19:30:33 +0300

    калькулятор

--- лабораторная работа 1/src/com/company/Main.java
+++ лабораторная работа 1/src/com/company/Main.java
@@ -1,50 +1,202 @@
 package com.company;
+import java.lang.String;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String arr[]){
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+        Deque<Double> chi = new ArrayDeque<>();
         Scanner in = new Scanner(System.in);
-        String S;
-        System.out.println("Введите комбинацию скобок: ");
-        S=in.nextLine();
-        char []ar=S.toCharArray();
-        Stack stack=new Stack<Character>();
-        if(ar[0] == ')' || ar[0] == ']' || ar[0] == '}' || ar.length % 2 != 0)
-        {
-            System.out.println("Неправильная комбинация");
-            return;
+        double sum = 0;
+        System.out.print("Введите арифметическое выражение\n");
+        String S = in.nextLine();
+        String[] arr = new String[S.length() + 100];
+        int i = 1;
+        int k = 1;
+        arr[0] = String.valueOf(S.charAt(0));
+        //Разбитие строки на элементы и запихивание в массив
+        while (k < S.length()) {
+            if ((arr[i - 1].charAt(0) >= '0' && arr[i - 1].charAt(0) <= '9' && S.charAt(k) >= '0' && S.charAt(k) <= '9')
+                    || (arr[i - 1].length() > 1 && ((arr[i - 1].charAt(1) >= '0' && arr[i - 1].charAt(1) <= '9' && S.charAt(k) >= '0' && S.charAt(k) <= '9')))
+                    || (i > 1 && S.charAt(k) >= '0' && S.charAt(k) <= '9' && arr[i - 1].charAt(0) == '-' && arr[i - 2].charAt(0) == '(')
+                    ||(S.charAt(k) >= '0' && S.charAt(k) <= '9'&&i==1&&arr[i-1].charAt(0)=='-')
+            ) {
+                arr[i - 1] = arr[i - 1] + S.charAt(k);
+            } else {
+                arr[i] = "" + S.charAt(k);
+                if ((arr[i - 1].charAt(0) <= '9' && arr[i - 1].charAt(0) >= '0')|| (arr[i - 1].length() > 1 && arr[i - 1].charAt(1) >= '0' && arr[i - 1].charAt(1) <= '9')) {
+                    chi.push(Double.parseDouble(arr[i - 1]));
+                } else {
+                    if (arr[i - 1].charAt(0) == ')') {
+                        boolean key = false;
+                        while (1 > 0) {
+                            if (stack.size() > 0 && stack.peek().equals("^")) {
+                                chi.push(Math.pow(chi.pop(),chi.pop()));
+
+                                stack.pop();
+                            }
+                            if (stack.size() > 0 && stack.peek().equals("+")) {
+                                chi.push(chi.pop() + chi.pop());
+
+                                stack.pop();
+                            }
+                            if (stack.size() > 0 && stack.peek().equals("-")) {
+                                chi.push(-chi.pop() + chi.pop());
+                                stack.pop();
+                            }
+                            if (stack.size() > 0 && stack.peek().equals("/")) {
+                                double pochka = chi.pop();
+                                chi.push(chi.pop() / pochka);
+                                stack.pop();
+                            }
+                            if (stack.size() > 0 && stack.peek().equals("*")) {
+                                chi.push(chi.pop() * chi.pop());
+                                stack.pop();
+                            }
+                            if (stack.peek().equals("(")) {
+                                stack.pop();
+                                break;
+                            }
+                        }
+                    }
+                    else  if (chi.size()>1&&stack.size() > 0 && stack.peek().equals("^")&&arr[i-1].charAt(0)!='(') {
+                        double zam=chi.pop();
+                        chi.push(Math.pow(chi.pop(),zam));
+                        stack.pop();
+                        if(arr[i-1].charAt(0)!='^'){stack.push(arr[i-1]);}
+                    }
+                    else if (stack.size() > 0 && (arr[i - 1].charAt(0) == '*' || arr[i - 1].charAt(0) == '/') && (stack.peek().equals("*") || stack.peek().equals("/"))) {
+                        if (stack.peek().equals("*")) {
+                            chi.push(chi.pop() * chi.pop());
+                            stack.pop();
+                        } else if (stack.peek().equals("/")) {
+                            double pack = chi.pop();
+                            chi.push(chi.pop() / pack);
+                            stack.pop();
+                        }
+                        stack.push(arr[i - 1]);
+                    } else if (arr[i - 1].charAt(0) == '(' || stack.size() < 1 || arr[i - 1].charAt(0) == '*' || arr[i - 1].charAt(0) == '/') {
+                        stack.push(arr[i - 1]);
+                    } else {
+                        if ((arr[i - 1].charAt(0) == '+' || arr[i - 1].charAt(0) == '-') && !stack.peek().equals("(")) {
+                            if (stack.size() > 0 && stack.peek().equals("+")) {
+                                chi.push(chi.pop() + chi.pop());
+
+                                stack.pop();
+                                stack.push(arr[i - 1]);
+                            } else if (stack.size() > 0 && stack.peek().equals("-")) {
+                                chi.push(-chi.pop() + chi.pop());
+                                stack.pop();
+                                stack.push(arr[i - 1]);
+                            } else if (stack.size() > 0 && stack.peek().equals("/")) {
+                                double pack = chi.pop();
+                                chi.push(chi.pop() / pack);
+                                stack.pop();
+                                stack.push(arr[i - 1]);
+                            } else if (stack.size() > 0 && stack.peek().equals("*")) {
+                                chi.push(chi.pop() * chi.pop());
+                                stack.pop();
+                                stack.push(arr[i - 1]);
+                            }
+                        } else stack.push(arr[i - 1]);
+                    }
+                }
+                i++;
+            }
+            k++;
         }
-        for(int i = 0;i<ar.length;i++)
-        {
-            switch (ar[i])
-            {
-                case '(' : stack.push(ar[i]); break;
-                case '[' : stack.push(ar[i]); break;
-                case '{' :stack.push(ar[i]); break;
-                case ')' : {
-                    if (stack.size()!=0) {
-                        if (stack.peek().equals('('))
-                            stack.pop();
+        if (arr[i - 1].charAt(0) <= '9' && arr[i - 1].charAt(0) >= '0') {
+            chi.push(Double.parseDouble(arr[i - 1]));
+        }
+        else {
+            if (arr[i - 1].charAt(0) == ')') {
+                boolean key = false;
+                while (1 > 0) {
+                    if (stack.size() > 0 && stack.peek().equals("+")) {
+                        chi.push(chi.pop() + chi.pop());
+
+                        stack.pop();
                     }
-                } break;
-                case '}' : {
-                    if(stack.size()!=0) {
-                        if (stack.peek().equals('{'))
-                            stack.pop();
+                    if (stack.size() > 0 && stack.peek().equals("-")) {
+                        chi.push(-chi.pop() + chi.pop());
+                        stack.pop();
                     }
-                } break;
-                case ']' : {
-                    if(stack.size()!=0) {
-                        if (stack.peek().equals('['))
-                            stack.pop();
+                    if (stack.size() > 0 && stack.peek().equals("/")) {
+                        double pochka = chi.pop();
+                        chi.push(chi.pop() / pochka);
+                        stack.pop();
                     }
-                } break;
+                    if (stack.size() > 0 && stack.peek().equals("*")) {
+                        chi.push(chi.pop() * chi.pop());
+                        stack.pop();
+                    }
+                    if (stack.peek().equals("(")) {
+                        stack.pop();
+                        break;
+                    }
+                }
+            }
+            else  if (stack.size() > 0 && stack.peek().equals("^")) {
+                double zam=chi.pop();
+                chi.push(Math.pow(chi.pop(),zam));
+                stack.pop();
+                if(arr[i-1].charAt(0)!='^'){stack.push(arr[i-1]);}
+            }
+            else if (arr[i - 1].charAt(0) == '(' || stack.size() < 1 || arr[i - 1].charAt(0) == '*' || arr[i - 1].charAt(0) == '/') {
+                stack.push(arr[i - 1]);
+            } else {
+                if ((arr[i - 1].charAt(0) == '+' || arr[i - 1].charAt(0) == '-') && !stack.peek().equals("(")) {
+
+                    if (stack.size() > 0 && stack.peek().equals("+")) {
+                        chi.push(chi.pop() + chi.pop());
+
+                        stack.pop();
+                    } else if (stack.size() > 0 && stack.peek().equals("-")) {
+                        chi.push(-chi.pop() + chi.pop());
+                        stack.pop();
+                    } else if (stack.size() > 0 && stack.peek().equals("/")) {
+                        double pack = chi.pop();
+                        chi.push(chi.pop() / pack);
+                        stack.pop();
+                    } else if (stack.size() > 0 && stack.peek().equals("*")) {
+                        chi.push(chi.pop() * chi.pop());
+                        stack.pop();
+                    }
+                } else stack.push(arr[i - 1]);
+            }
+
+        }
+        while (stack.size() > 0) {
+            if (stack.size() > 0 && stack.peek().equals("^")) {
+                double zam=chi.pop();
+                chi.push(Math.pow(chi.pop(),zam));
+                stack.pop();
+
+            }
+            if (stack.size() > 0 && stack.peek().equals("+")) {
+                chi.push(chi.pop() + chi.pop());
+
+                stack.pop();
+            }
+            if (stack.size() > 0 && stack.peek().equals("-")) {
+                chi.push(-chi.pop() + chi.pop());
+                stack.pop();
+            }
+            if (stack.size() > 0 && stack.peek().equals("/")) {
+                double pack = chi.pop();
+                chi.push(chi.pop() / pack);
+                stack.pop();
+            }
+            if (stack.size() > 0 && stack.peek().equals("*")) {
+                chi.push(chi.pop() * chi.pop());
+                stack.pop();
             }
         }
-        if(stack.size()==0)
-            System.out.println("Введена правильная комбинация скобок");
-        else
-            System.out.println("Введена неправильная комбинация скобок");
+        System.out.print("Результат:\n"+S+"="+chi.peek());
     }
 }
